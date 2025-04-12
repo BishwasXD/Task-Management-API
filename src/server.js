cfa265c7c92885588ea38407a195ee28941c2395
BishwasXD <gautambishwas7@gmail.com>
2025-04-12 14:58:51 +0545

    basic server setup

---
+++ src/server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.listen(port, ()=>{
+  console.log("APP IS LISTENING ON PORT", port)
+})
+
+app.get('/', (req, res) => {
+  res.send("Hello world")
+})
+
+
+
+
